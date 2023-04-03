c2931ba9b0f12cc9ca32d537b119953232038a65
Christian Henning <122561083+c4henning@users.noreply.github.com>
2023-04-02 23:13:20 -0400

    PHP version of nav.html
    This version of the navbar uses PHP to dynamically switch between file paths for development and live site.

---
+++ Group Pages/partials/nav.php
@@ -0,0 +1,43 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div class="container-fluid">
+    <a href="<?php $base_url ?>/index.html" class="navbar-brand">
+      <span class="nav-full-text navbar-brand d-none d-sm-inline">National Lacrosse League</span>
+      <span class="nav-short-text navbar-brand d-sm-none d-inline">NLL</span>
+    </a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <div class="navbar-nav ms-auto">
+        <a href="<?php $base_url ?>/about.html" class="nav-link">About</a>
+        <a href="<?php $base_url ?>/catalog.html" class="nav-link">Catalog</a>
+        <a href="<?php $base_url ?>/references.html" class="nav-link">References</a>
+        <a href="<?php $base_url ?>/MORE/SCHEDULE.html" class="nav-link">Schedule</a>
+        <div class="nav-item dropdown">
+          <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">Teams</a>
+          <div class="dropdown-menu dropdown-menu-dark dropdown-menu-end">
+            <a href="<?php $base_url ?>/EAST_TEAMS/EAST_TEAMS.html" class="dropdown-item">East Teams</a>
+            <hr class="dropdown-divider">
+            <a href="<?php $base_url ?>/WEST_TEAMS/WEST_TEAMS.html" class="dropdown-item">West Teams</a>
+          </div>
+        </div>
+        <div class="nav-item dropdown">
+          <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">More</a>
+          <div class="dropdown-menu dropdown-menu-dark dropdown-menu-end">
+            <a href="<?php $base_url ?>/MORE/BRACKETS.html" class="dropdown-item">Bracket Creator</a>
+            <hr class="dropdown-divider">
+            <a href="<?php $base_url ?>/MORE/HISTORY.html" class="dropdown-item">History</a>
+            <hr class="dropdown-divider">
+            <a href="<?php $base_url ?>/MORE/RULES.html" class="dropdown-item">Rules</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</nav>
+
+<?php
+// Dynamic switching of file paths between development and live site
+$base_url = 'http://' . $_SERVER['HTTP_HOST'] . dirname(dirname($_SERVER['PHP_SELF']));
+?>
